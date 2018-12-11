894c3d81edbb326fd3291a29681bdaeaf16e9f29
banteg <banteeg@gmail.com>
2018-12-12 03:58:38 +0700

    add get exchanges from logs

--- uniswap/contracts.py
+++ uniswap/contracts.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from web3.auto import w3
 from web3.exceptions import BadFunctionCallOutput
 
+deployed = 6627917
 deployments = {
     '1': '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
     '4': '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36',
@@ -26,6 +27,12 @@ def get_exchange(factory, token_address):
         factory.functions.getExchange(token_address).call(),
         abi=abi['exchange']
     )
+
+
+def get_exchanges(factory):
+    # token -> exchange
+    filt = factory.events.NewExchange().createFilter(fromBlock=deployed)
+    return {x['args']['token']: x['args']['exchange'] for x in filt.get_all_entries()}
 
 
 def get_token(address):
