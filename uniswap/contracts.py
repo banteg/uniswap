015d0757021a13e41d22a00607e485c82d889c5f
banteg <banteeg@gmail.com>
2018-12-11 13:51:20 +0700

    update contracts

--- uniswap/contracts.py
+++ uniswap/contracts.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 from web3.auto import w3
 from web3.exceptions import BadFunctionCallOutput
@@ -7,28 +8,30 @@ deployments = {
     '1': '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
     '4': '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36',
 }
+abi = {
+    f.stem: json.loads(f.read_text()) for f
+    in Path('abi').iterdir() if f.suffix == '.json'
+}
 
 
 def get_factory():
     return w3.eth.contract(
         deployments[w3.version.network],
-        abi=json.load(open('abi/factory.json'))
+        abi=abi['factory']
     )
 
 
 def get_exchange(factory, token_address):
     return w3.eth.contract(
         factory.functions.getExchange(token_address).call(),
-        abi=json.load(open('abi/exchange.json'))
+        abi=abi['exchange']
     )
 
 
 def get_token(address):
     try:
-        erc20_abi = json.load(open('abi/erc20.json'))
-        token = w3.eth.contract(address, abi=erc20_abi)
+        token = w3.eth.contract(address, abi=abi['erc20'])
         token.functions.symbol().call()
     except (OverflowError, BadFunctionCallOutput, ValueError):
-        erc20_bytes_abi = json.load(open('abi/erc20_symbol_bytes32.json'))
-        token = w3.eth.contract(address, abi=erc20_bytes_abi)
+        token = w3.eth.contract(address, abi=abi['erc20_symbol_bytes32'])
     return token
