d46aac342e0d7045342bd973700f90b1e9b5bce4
banteg <banteeg@gmail.com>
2019-03-01 10:38:35 +0700

    add faster parallel indexer

--- uniwatch/indexer.py
+++ uniwatch/indexer.py
@@ -51,27 +51,24 @@ async def get_exchanges() -> [Exchange]:
     return [Exchange(*row) for row in exchanges]
 
 
-async def index_exchange_logs(exchange: Exchange, step=4096):
-    sql = 'select last_block + 1 from exchanges where exchange = $1'
-    start = await db.fetchval(sql, exchange.exchange) or exchange.block
+async def index_parallel(exchanges: [Exchange], step=4096):
+    addresses = {x.exchange for x in exchanges}
+    start = await db.fetchval('select max(block) + 1 from events') or uniswap.genesis
     last = w3.eth.blockNumber
     for offset in trange(start, last, step):
         to_block = min(offset + step - 1, last)
-        params = filter_params(exchange.exchange, from_block=offset, to_block=to_block)
+        params = filter_params(addresses, from_block=offset, to_block=to_block)
         batch = w3.eth.getLogs(params)
         decoded = decode_logs(batch)
         events = [Event.from_log(log) for log in decoded]
         for event in events:
             await event.save()
-        await exchange.update_last_block(to_block)
 
 
 async def main():
     await db.init()
     exchanges = await get_exchanges()
-    for i, e in enumerate(exchanges, 1):
-        print(i, e)
-        await index_exchange_logs(e)
+    await index_parallel(exchanges)
 
 
 '''
