7962c511d1848adad3fc4c1daf1b29f6f2eef89f
banteg <banteeg@gmail.com>
2019-03-01 08:48:03 +0700

    add event model

--- uniwatch/models.py
+++ uniwatch/models.py
@@ -1,4 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, astuple
+
+from uniwatch.db import db
 
 
 @dataclass
@@ -10,3 +12,23 @@ class Exchange:
     @classmethod
     def from_log(cls, log):
         return cls(token=log.args.token, exchange=log.args.exchange, block=log.blockNumber)
+
+
+
+@dataclass
+class Event:
+    exchange: str
+    event: str
+    data: dict
+    block: int
+    log_index: int
+
+    @classmethod
+    def from_log(cls, log):
+        return cls(log.address, log.event, dict(log.args), log.blockNumber, log.logIndex)
+
+    async def save(self):
+        await db.execute(
+            'insert into events (exchange, event, data, block, log_index) values ($1, $2, $3, $4, $5)',
+            *astuple(self)
+        )
